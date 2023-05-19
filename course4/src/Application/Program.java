a822e38244f0ff7ce2045de26f96b4d4d9e91adf
Lucas Alves <Lucas Alves@192.168.1.6>
2023-05-19 16:17:57 -0300

    commit

---
+++ course4/src/Application/Program.java
@@ -0,0 +1,37 @@
+package Application;
+
+import java.util.Locale;
+import java.util.Scanner;
+import entities.Employee;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Employee emp = new Employee();
+		
+		System.out.println("Name: ");
+		emp.name = sc.nextLine();
+		System.out.println("Gross salary: ");
+		emp.grossSalary = sc.nextDouble();
+		System.out.println("Tax: ");
+		emp.tax = sc.nextDouble();
+		
+		System.out.println();
+		System.out.println("Emplyee: " + emp);
+		System.out.println();
+		System.out.println("Wich percentage to increse salary?");
+		double percentage = sc.nextDouble();
+		emp.increaseSalary(percentage);
+		
+		System.out.println();
+		System.out.println("Updated data: " + emp);
+		
+		sc.close();
+		
+	}
+
+}
